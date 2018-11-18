f207d3b1ce2ee0fd531f43e9d8749f588f08a5f4
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-11-18 20:56:22 +0100

    add properties to user object

--- server/seeders/20180503174251-users.js
+++ server/seeders/20180503174251-users.js
@@ -1,15 +1,18 @@
-import bcrypt from 'bcrypt';
+//import bcrypt from 'bcrypt';
+const bcrypt = require('bcrypt');
 
-export default {
+module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', [
     {
       userId: 'e20ac257-86cc-4a6f-a619-0249a201c475',
       firstname: 'Admin',
       lastname: 'Admin',
       email: 'admin@wizzyagro.com',
-      username: 'otseobande',
-      password: bcrypt.hashSync('admin', 12),
-      confirmed: true
+      password: bcrypt.hashSync('admin', 10),
+      confirmed: true,
+      token: '111111',
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
   ], {}),
 
