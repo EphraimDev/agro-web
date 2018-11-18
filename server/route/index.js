b52d16fd5649d51a8471a9dfdb43594a9c939b5e
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-11-18 22:49:16 +0100

    make changes to write article route

--- server/route/index.js
+++ server/route/index.js
@@ -13,7 +13,7 @@ let storage = multer.diskStorage({
          cb(null, './uploads/');
      },
      filename: function(req, file, cb){
-       cb(null, new Date().toISOString().replace(/:/g, '-'), file.originalname);
+       cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
      }
    })
 
