0c5ccf9e7633d0b2dfc47c416d0a395c60e235af
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-11-19 10:06:27 +0100

    add articles seed

--- server/seeders/20180503174347-articles.js
+++ server/seeders/20180503174347-articles.js
@@ -16,13 +16,17 @@ module.exports = {
       Rubber is the second-largest non-oil foreign exchange earner.
       
       Oil Palm
-      The palms industry constitutes a significant sector of the Nigerian economy, providing food and raw materials for the Food, Cosmetics, Pharmaceuticals, Plastics and the Bio-energy industries. In Nigeria the institute that has valuable information about oil palm is the Nigerian Institute for Oil Palm Research. The formal mandate of the institute is to conduct research into the production and products of oil palm and other palms of economic importance and transfer its research findings to farmers.[17]`
+      The palms industry constitutes a significant sector of the Nigerian economy, providing food and raw materials for the Food, Cosmetics, Pharmaceuticals, Plastics and the Bio-energy industries. In Nigeria the institute that has valuable information about oil palm is the Nigerian Institute for Oil Palm Research. The formal mandate of the institute is to conduct research into the production and products of oil palm and other palms of economic importance and transfer its research findings to farmers.[17]`,
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     {
       articleId: 'ba8e1fd3-926f-44c9-a7b3-218aedab8c12',
       title: 'Rice farming',
       image: 'https://i.imgur.com/6uLB90p.jpg',
-      article: `Interestingly, Nigeria, which is the largest producer of rice in West Africa and the third in Africa after Egypt and Madagascar producing about 3 million metric tons on the average annually, falls short of meeting its local demand which is placed at about 5 million tons. This particular statistic makes her the highest consumer of rice in the West African sub region and the second largest importer in the world, buying at least 2 million tons annually. Massive importation of the product from countries like India, China, Thailand etc, therefore, occur largely on account of the fact that the estimated amount of rice milled locally is placed at 1.8 million tons. On the average, Nigeria spends 1 billion Naira on rice importation daily (that’s a grueling 365 billion Naira annually). Agriculturalists keep asking ”why spend such an outrageous amount of money importing rice when Nigeria has the potential of growing enough rice to support Her population and generate surplus that can be exported?”.`
+      article: `Interestingly, Nigeria, which is the largest producer of rice in West Africa and the third in Africa after Egypt and Madagascar producing about 3 million metric tons on the average annually, falls short of meeting its local demand which is placed at about 5 million tons. This particular statistic makes her the highest consumer of rice in the West African sub region and the second largest importer in the world, buying at least 2 million tons annually. Massive importation of the product from countries like India, China, Thailand etc, therefore, occur largely on account of the fact that the estimated amount of rice milled locally is placed at 1.8 million tons. On the average, Nigeria spends 1 billion Naira on rice importation daily (that’s a grueling 365 billion Naira annually). Agriculturalists keep asking ”why spend such an outrageous amount of money importing rice when Nigeria has the potential of growing enough rice to support Her population and generate surplus that can be exported?”.`,
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     {
       articleId: 'fa56c9e7-e5f4-4086-b7e9-db581201b71f',
@@ -32,7 +36,9 @@ module.exports = {
 
       Cassava grows very well in most parts of West Africa like Ghana, Senegal, Benin Republic, Nigeria, and Cameroon. The Brazilians are also know for cassava farming which they use for Tapioca production.
       
-      The industrial processing of cassava in Nigeria holds much potential for successful investment.  The crop is increasingly attracting attention of serious business people. And processing businesses are slowly starting to spring-up all over the country. You also know how useful cassava is to Nigerians in general in terms of consumption. Cassava forms the major part of our daily food. Almost no family can survive  a whole year without cassava related food.`
+      The industrial processing of cassava in Nigeria holds much potential for successful investment.  The crop is increasingly attracting attention of serious business people. And processing businesses are slowly starting to spring-up all over the country. You also know how useful cassava is to Nigerians in general in terms of consumption. Cassava forms the major part of our daily food. Almost no family can survive  a whole year without cassava related food.`,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
   ], {}),
 
