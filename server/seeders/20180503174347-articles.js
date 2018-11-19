0cac7fdfa4cc65896c0c30d8ded82180f7807778
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-11-19 18:39:04 +0100

    make changes to cors and article type

--- server/seeders/20180503174347-articles.js
+++ server/seeders/20180503174347-articles.js
@@ -20,26 +20,6 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     },
-    {
-      articleId: 'ba8e1fd3-926f-44c9-a7b3-218aedab8c12',
-      title: 'Rice farming',
-      image: 'https://i.imgur.com/6uLB90p.jpg',
-      article: `Interestingly, Nigeria, which is the largest producer of rice in West Africa and the third in Africa after Egypt and Madagascar producing about 3 million metric tons on the average annually, falls short of meeting its local demand which is placed at about 5 million tons. This particular statistic makes her the highest consumer of rice in the West African sub region and the second largest importer in the world, buying at least 2 million tons annually. Massive importation of the product from countries like India, China, Thailand etc, therefore, occur largely on account of the fact that the estimated amount of rice milled locally is placed at 1.8 million tons. On the average, Nigeria spends 1 billion Naira on rice importation daily (that’s a grueling 365 billion Naira annually). Agriculturalists keep asking ”why spend such an outrageous amount of money importing rice when Nigeria has the potential of growing enough rice to support Her population and generate surplus that can be exported?”.`,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      articleId: 'fa56c9e7-e5f4-4086-b7e9-db581201b71f',
-      title: 'Cassava Farming',
-      image: 'https://i.imgur.com/BmZVuX9.jpg',
-      article: `It is unfortunate to see many Nigerians go to bed hungry, suffering of starvation and malnutrition. We constantly complain of lack of good paying jobs and no money to spend and take care of our immediate needs. In effort to get jobs, we sell themselves into slavery in the hands of Chinese and Lebanese right in their own country all in the name of working in a factory. While in reality, we are sitting on a goldmine known as cassava farming and production!
-
-      Cassava grows very well in most parts of West Africa like Ghana, Senegal, Benin Republic, Nigeria, and Cameroon. The Brazilians are also know for cassava farming which they use for Tapioca production.
-      
-      The industrial processing of cassava in Nigeria holds much potential for successful investment.  The crop is increasingly attracting attention of serious business people. And processing businesses are slowly starting to spring-up all over the country. You also know how useful cassava is to Nigerians in general in terms of consumption. Cassava forms the major part of our daily food. Almost no family can survive  a whole year without cassava related food.`,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
   ], {}),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Articles', null, {})
