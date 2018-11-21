4d4a7b59b1f52da96f122740fe104e07751e41e0
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-11-21 14:20:02 +0100

    change cors settings

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-//import cors from 'cors';
+import cors from 'cors';
 
 // Set up the express app
 const app = express();
@@ -15,19 +15,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.headers.origin) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-  }
-  if (req.method === 'OPTIONS') {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-//app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   if (req.headers.origin) {
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//   }
+//   if (req.method === 'OPTIONS') {
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+app.use(cors());
+app.options('*', cors())
 app.use(router);
 
 
