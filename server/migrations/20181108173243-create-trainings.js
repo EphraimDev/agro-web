e6c29cba034131d20b53decacda16f11e1a11681
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2019-01-01 07:25:50 +0100

    Add all  migrations

---
+++ server/migrations/20181108173243-create-trainings.js
@@ -0,0 +1,65 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Trainings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      trainingsId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+      },
+      training: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      location: {
+        type: Sequelize.ENUM('Virtual', "At Clients Place", "At Wizzy Agro Farm"),
+        allowNull: false,
+        defaultValue: 'Virtual'
+      },
+      status: {
+        type: Sequelize.ENUM('Canceled', 'Pending', 'Started', 'Ended'),
+        allowNull: false,
+        defaultValue: 'Pending'
+      },
+      duration: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      comment: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      userId: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'userId',
+          as: 'userId'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Trainings');
+  }
+};
